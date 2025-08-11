052fdd63ea81f54d5cff5fad3aa67cf762d44ce3
DonovanJJ <donovanchanjiajun2001@gmail.com>
2025-08-11 23:45:36 +0800

    fix networkng

--- dht-esp-dashboard/src/clients/device.tsx
+++ dht-esp-dashboard/src/clients/device.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type {Device} from "../models/Device.ts";
 
-const END_POINT = "localhost:3000"
+const END_POINT = "backend:3000"
 
 export async function getAvailableDevices(): Promise<Device[]> {
   const url = `http://${END_POINT}/devices`
